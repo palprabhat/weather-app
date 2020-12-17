dfe92a7e73b4eba34629371ac0f9117e91638f73
Prabhat Pal <prabhat.pal@tantrumstreet.com>
2020-12-17 11:34:55 -0800

    updated caching strategies

--- public/sw.js
+++ public/sw.js
@@ -3,15 +3,53 @@ importScripts(
 );
 
 workbox.routing.registerRoute(
+  /.*\/api\/.*$/,
+  new workbox.strategies.NetworkFirst({
+    cacheName: "apis",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 500,
+        maxAgeSeconds: 1 * 60 * 60,
+      }),
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 365 * 24 * 60 * 60,
+      }),
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
   ({ request }) =>
     request.destination === "style" ||
     request.destination === "script" ||
     request.destination === "worker" ||
     request.destination === "document",
-  new workbox.strategies.StaleWhileRevalidate()
-);
-
-workbox.routing.registerRoute(
-  ({ request }) => request.destination === "image",
-  new workbox.strategies.CacheFirst()
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "assets",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 500,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [200],
+      }),
+    ],
+  })
 );
