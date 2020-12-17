6b1dee0dcd3ebef13e48a0e07a1e46220afacf7b
Prabhat Pal <prabhat.pal@tantrumstreet.com>
2020-12-17 10:42:25 -0800

    log errors

--- pages/api/autocomplete.js
+++ pages/api/autocomplete.js
@@ -14,7 +14,8 @@ export default async (req, res) => {
 
       if (response.status !== 200 || response.data.status !== "OK") {
         res.status(500).send({
-          error: "Unable to get autpcomplete",
+          error: "Unable to get autocomplete",
+          response: response,
           message: response.data.error_message,
         });
         return;
@@ -23,7 +24,7 @@ export default async (req, res) => {
       res.status(200).send(response.data);
     } catch (err) {
       console.error(err);
-      res.status(500).send({ error: "Unable to get autpcomplete" });
+      res.status(500).send({ error: "Unable to get autocomplete" });
     }
   } else {
     res.status(405).send({ error: "Request method not supported" });
